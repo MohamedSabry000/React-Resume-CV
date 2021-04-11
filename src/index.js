2780e7bf61f0351bcdd887d97ee7174f5ac1fb3e
Mohamedfadah <30502846+Mohamedfadah@users.noreply.github.com>
2021-04-11 12:57:44 +0200

    Upload React-Resume-CV Part 3

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Template CSS Files 
+import './css/font-awesome.min.css';
+import './css/jquery.animatedheadline.css';
+import './css/materialize.min.css';
+import './css/style.css';
+import './css/skins/yellow.css';
+
+import App from './components/app/App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render( <App />, document.getElementById('root'));
+
+reportWebVitals();
